220c22f9bce7edf823b8bda1bffc8d3fb00f63ec
Miyatoshi007 <ninhnapc09333@gmail.com>
2025-03-22 13:51:02 +0700

    Update Contact.jsx

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <div id="contact" className="bg-amber-400">
-            This is contact
+            This is contact page "New test deploy"
         </div>
     )
 }
